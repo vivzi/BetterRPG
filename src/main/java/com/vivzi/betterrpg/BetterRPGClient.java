6513cee33caa2b927ac30c31b6460a1e2ff2bfed
vivzi <63642761+vivzi@users.noreply.github.com>
2025-09-17 10:09:56 +0530

    Initial commit

---
+++ src/main/java/com/vivzi/betterrpg/BetterRPGClient.java
@@ -0,0 +1,10 @@
+package com.vivzi.betterrpg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterRPGClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
