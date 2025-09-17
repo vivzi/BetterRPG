87e4e162d987aca720d49a95d1fe52f05a90b7dd
vivzi <63642761+vivzi@users.noreply.github.com>
2025-09-17 11:04:45 +0530

    Added new item 'ruby'

--- src/main/java/com/vivzi/betterrpg/BetterRPG.java
+++ src/main/java/com/vivzi/betterrpg/BetterRPG.java
@@ -1,5 +1,6 @@
 package com.vivzi.betterrpg;
 
+import com.vivzi.betterrpg.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,7 +19,6 @@ public class BetterRPG implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
 	}
 }
